9541e8d05e1d8d525ee5f68a9f044731bb36ba59
AmrMonzir <amrmonzir@hotmail.com>
2025-01-14 02:57:02 +0000

    created search bar and allowed previews

---
+++ composeApp/src/androidMain/kotlin/com/plcoding/bookpedia/Previews.kt
@@ -0,0 +1,31 @@
+package com.plcoding.bookpedia
+
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.plcoding.bookpedia.book.presentation.book_list.components.BookSearchBar
+
+
+@Preview(backgroundColor = 0xFFFFFF)
+@Composable
+private fun BookSearchBarPreview() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.White)
+    ) {
+        BookSearchBar(
+            searchQuery = "Search",
+            onSearchQueryChange = {},
+            onSearch = {},
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
+}
+
